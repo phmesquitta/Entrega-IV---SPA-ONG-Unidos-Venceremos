96b0ef277f3c30957ad298a4fe9268a0e602d3f6
Paulo Henrique Silva Mesquita <paulohrick.95@gmail.com>
2025-11-14 22:13:33 -0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-import { navigate } from "./router.js";
-
-window.navigate = navigate;
-
-document.addEventListener("DOMContentLoaded", () => {
-  const ultima = localStorage.getItem("pagina") || "home";
-  navigate(ultima);
-});
