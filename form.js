bb81157de83f3fa08ba734c3042e8a3df188be0a
Paulo Henrique Silva Mesquita <paulohrick.95@gmail.com>
2025-11-14 22:13:23 -0300

    Delete form.js

--- form.js
+++
@@ -1,8 +0,0 @@
-export function validarFormulario() {
-  const form = document.getElementById('contatoForm');
-  if (!form) return;
-  form.addEventListener('submit', e=>{
-    e.preventDefault();
-    alert('Mensagem enviada! (simulação)');
-  });
-}
